ca1ddf97869a9b11a05eeb029c0bb0f0019cbafd
Cranch <50642181+Cranch-fur@users.noreply.github.com>
2021-09-08 20:36:44 +0300

    [1.0.0.0]

---
+++ CranchyLib.SaveFile/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CranchyLib.SaveFile
+{
+    public static class Extensions
+    {
+        public static T[] Subset<T>(this T[] array, int start, int count)
+        {
+            T[] array2 = new T[count];
+            Array.Copy(array, start, array2, 0, count);
+            return array2;
+        }
+    }
+}
